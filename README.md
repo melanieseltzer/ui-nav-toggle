87ac7397678c0338b8b545a3f83b6ef54d612e55
Melanie Seltzer <melleh11@gmail.com>
2017-09-21 16:50:53 -0700

    All files

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# ui-nav-toggle
-A fixed position nav bar with a button that toggles a widget of sorts.
+# UI - Fixed nav bar with button toggle
+
+A fixed position nav bar with a button that toggles a widget of sorts. Indicates open/close state. Widget itself is not fixed (optimal for mobile). When open on mobile, widget is 100% width and height is calculated using CSS calc (for optimal swiping).
+
+## Getting Started
+
+Uses Middleman for local development. Open terminal in folder and run using:
+
+```
+middleman
+```
+Now you can preview in your browser at:
+
+```
+http://localhost:4567/
+```
+
+## Deployment
+
+Create a build folder for production (minified files) using:
+
+```
+middleman build
+```
+
+## Built With
+
+* [Middleman](https://middlemanapp.com/) - The build tool
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ source/javascripts/all.js
@@ -0,0 +1,60 @@
+// Booking Widget Functionality
+$(document).ready(
+  function(){
+  // Trigger the booking widget to appear on Book Now click
+  $("#trigger-cta").click(function () {
+    // Toggle on booking widget
+    // Toggle on triangle
+    // Toggle on X icon, toggle off Book Now text
+    $("#toggle-widget").toggle();
+    $(".toggle-widget-triangle").toggle();
+    $("#trigger-cta span").toggle();
+    $("#trigger-cta i").toggleClass("fa fa-times");
+
+    // While booking widget is open
+    // If the user was still on button and their mouse leaves via somewhere NOT the widget
+    // Make sure to close widget so they don't have to click it close themselves
+    $("#trigger-cta").mouseleave(function(){
+      if ($('#toggle-widget').css('display') == 'block') {
+        // Toggle off booking widget
+        // Toggle off triangle
+        // Toggle off X icon, toggle on Book Now text
+        $("#toggle-widget").toggle();
+        $(".toggle-widget-triangle").toggle();
+        $("#trigger-cta span").toggle();
+        $("#trigger-cta i").toggleClass("fa fa-times");
+      }
+    });
+  });
+  
+  // Keep booking widget up while mouse is hovered over it
+  // Hide booking widget when mouse stops hovering
+  $("#toggle-widget").hover(function(){
+    // Toggle on booking widget
+    // Toggle on triangle
+    // Toggle on X icon, toggle off Book Now text
+    $(this).toggle();
+    $(".toggle-widget-triangle").toggle();
+    $("#trigger-cta span").toggle();
+    $("#trigger-cta i").toggleClass("fa fa-times");
+  },function(){
+    // Toggle off booking widget
+    // Toggle off triangle
+    // Toggle off X icon, toggle on Book Now text
+    $(this).toggle();
+    $(".toggle-widget-triangle").toggle();
+    $("#trigger-cta span").toggle();
+    $("#trigger-cta i").toggleClass("fa fa-times");
+  });
+
+  // Also keep showing booking widget while mouse is hovered over triangle decoration
+  $(".toggle-widget-triangle").hover(function(){
+    // Toggle on booking widget
+    // Toggle on triangle
+    // Toggle on X icon, toggle off Book Now text
+    $("#toggle-widget").toggle();
+    $(".toggle-widget-triangle").toggle();
+    $("#trigger-cta span").toggle();
+    $("#trigger-cta i").toggleClass("fa fa-times");
+  });
+});
